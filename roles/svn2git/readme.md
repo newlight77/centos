331c02f1fc5a809db8285cf0044163c33ed4f91e
Kong To <newlight77@gmail.com>
2020-03-22 18:52:05 +0100

    finalize the svn2git role with guidelines

--- roles/svn2git/readme.md
+++ roles/svn2git/readme.md
@@ -34,10 +34,23 @@ ansible-playbook ansible/playooks/playbook-svn2git.yml
 ```sh
 cd /appli/svn2git
 
-docker-compose up –d
+export SVN_URL=your_svn_url
+export GIT_URL=your_git_url
 
-docker exec svn2git /appli/svn2git/migrate.sh
+docker-compose up
+
+docker-compose down
+
+# in case you want to run that inside the containwer, you override the env variables 
+    # sh /appli/migrate.sh -s=your_svn_url -g=your_git_url
+# you need to change the docker-compose.yml : command: bash
+
 ```
 
 The log file resulting from the script execution will be available at [/apps/logs/svn2git-TIMESTAMP.log].
+
+## Reference
+
+[svn2git migration guide](https://github.com/newlight77/centos/tree/master/docs/svn2git-migration-guide.md)
+[git svn guide](https://github.com/newlight77/centos/tree/master/docs/git-svn-guide.md)
 
--- roles/svn2git/templates/migrate.sh
+++ roles/svn2git/templates/migrate.sh
@@ -22,10 +22,10 @@ createProjectDir() {
 convertBranches() {
   # Checkout all branches locally
   git for-each-ref --format="%(refname)" refs/remotes | grep -v trunk | cut -d / -f 2- | \
-  while read ref \
-  do \
-    branchName=`echo $ref | cut -f3 -d/` \
-    git checkout -b $branchName $ref \
+  while read ref 
+  do 
+    branchName=`echo $ref | cut -f3 -d/` 
+    git checkout -b $branchName $ref 
   done
 }
 
@@ -33,25 +33,25 @@ convertTags() {
 
   # Convert 'SVN tags' branches to simple git tags
   git for-each-ref --format="%(refname:short) %(objectname)" refs/remotes/origin/tags | cut -d / -f 3- | \
-  while read ref \
-  do \
-    git tag -a $ref -m 'import tag from svn' \
+  while read ref 
+  do 
+    git tag -a $ref -m 'import tag from svn' 
   done
 
   # Delete SVN tag branches
   git for-each-ref --format="%(refname:short)" refs/remotes/origin/tags | cut -d / -f 1- | \
   while read ref
   do
-    git branch -rd $ref \
+    git branch -rd $ref 
   done
 
   # Since tags marked in the previous step point to a commit "create tag", we need to derive "real" tags, i.e. parents of "create tag" commits.
   git for-each-ref --format="%(refname:short)" refs/tags | \
-  while read ref \
-  do \
-    tag=`echo $ref | sed 's/_/./g'` # give tags a new name \
-    echo $ref -\> $tag \
-    git tag -f -a $tag `git rev-list -2 $ref | tail -1` -m "proper svn tag" \
+  while read ref 
+  do 
+    tag=`echo $ref | sed 's/_/./g'` # give tags a new name 
+    echo $ref -\> $tag 
+    git tag -f -a $tag `git rev-list -2 $ref | tail -1` -m "proper svn tag" 
   done
 
 }
@@ -62,8 +62,8 @@ convertTags() {
 for i in "$@"
   do
     case $i in
-      -p=*|--svnUrl=*)       SVN_URL="${i#*=}"  ;;
-      -p=*|--gitUrl=*)       GIT_URL="${i#*=}"  ;;
+      -s=*|--svnUrl=*)       SVN_URL="${i#*=}"  ;;
+      -g=*|--gitUrl=*)       GIT_URL="${i#*=}"  ;;
       -a|--all)               ALL="true"         ;;
       -h|--help)                   usage               ;;
       *)                           usage               ;;
@@ -78,13 +78,16 @@ done
 mkdir logs
 
 echo 'Create a new git repository pointing to an existing Svn repository'
+ 
+echo "SVN_URL=$SVN_URL"
+echo "GIT_URL=$GIT_URL"
 
-git svn init $SVN_URL . -T trunk -t tags -b branches
+git svn init $SVN_URL . -T trunk -t tags -b branches | tee logs/svn2git-$(date +%F+%T).log 
 git svn fetch | tee logs/svn2git-$(date +%F+%T).log 
 
 echo 'Merging Svn history to Git history'
 # Apply commits to develop branch
-git checkout -b develop
+git checkout -b develop  | tee logs/svn2git-$(date +%F+%T).log 
 # Retrieve all the changes from the Svn
 git svn rebase | tee logs/svn2git-$(date +%F+%T).log 
 
@@ -98,7 +101,7 @@ convertBranches | tee logs/svn2git-$(date +%F+%T).log
 git config --global http.sslVerify false
 
 echo 'Push all tags and branches to Git remote'
-git remote add new-origin https://$GIT_URL
+git remote add new-origin $GIT_URL
 git push new-origin --all --force | tee logs/svn2git-$(date +%F+%T).log 
 git push new-origin --tags | tee logs/svn2git-$(date +%F+%T).log 
 
